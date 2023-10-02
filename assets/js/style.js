9353cc194667efe291b3a085e60bece42984baa8
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-10-02 15:41:28 -0400

    API and JS updates

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,2 +1,11 @@
-$(document).ready(function () {
-};
+var key = "9b9f310efa391a785a8ba71112f98ed7"; //API KEY
+var city = "Richmond";
+
+
+//current time and date
+var currentDate = moment(). format('dddd, MMMM Do YYYY');
+var currentTime = moment(). format('YYYY-MM-DD HH:MM:SS');
+
+
+// $(document).ready(function () {
+// };
