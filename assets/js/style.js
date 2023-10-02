347ca1c4518dba11da3ea599d7371f385d4f2fa8
Christine Campbell <christiecamp@Christines-MacBook-Pro.local>
2023-10-02 16:49:22 -0400

    working through js, html, and css - creating functions

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,11 +1,46 @@
 var key = "9b9f310efa391a785a8ba71112f98ed7"; //API KEY
-var city = "Richmond";
-
+var city = "Richmond"; //base city
 
 //current time and date
 var currentDate = moment(). format('dddd, MMMM Do YYYY');
 var currentTime = moment(). format('YYYY-MM-DD HH:MM:SS');
 
+$(document).ready(function () {
+    
+//city searched list
+var cityList = [];
+$('.search').on("click", function (event){
+    event.preventDefault();
+    city = $(this).parent('.btn').siblings('.textVal').val().trim();
+    if (city === "") { 
+        return;
+    };
+    //saves searched city into local storage
+    localStorage.setItem('city', JSON.stringify(cityList));
+    tossCoin ();
 
-// $(document).ready(function () {
-// };
+});
+//function that will fetch data from open weather API
+function tossCoin (city) {
+    var url =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&appid="+ key;
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.then (function (data){
+    console.log(data);
+    localStorage.setItem("city",JSON.stringify(data));
+    console.log(data);
+
+//setting the current city on the page
+var cityCurrent = document.getElementById ("current-city");
+cityCurrent.textContent = city;
+//function for getting weather
+})
+}
+
+
+});
